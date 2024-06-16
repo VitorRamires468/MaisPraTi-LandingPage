ae883ec22cf24f9aa6ec11e807b4bc0a505c82d1
vitorramires468 <ramiresvitor468@gmail.com>
2024-06-16 13:51:46 -0300

    Novas secoes

---
+++ script.js
@@ -0,0 +1,9 @@
+function saberMais() {
+  let pagina = document.getElementById("meuModal");
+  pagina.style.display = "block";
+}
+function fecharModal() {
+  let pagina = document.getElementById("meuModal");
+  pagina.style.display = "none";
+  window.location.href = "https://www.starwars.com/";
+}
